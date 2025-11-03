6e3e9cbb2a4b54230b34c8247c0836e98106e6a1
Victor Abuka <abukavictoro@gmail.com>
2025-11-03 12:05:09 +0100

    Add cors policy

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ var mySwitch bool = false
 
 func main() {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/switch", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": mySwitch,
@@ -20,4 +21,20 @@ func main() {
 		})
 	})
 	router.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
